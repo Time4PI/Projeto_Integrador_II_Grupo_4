951feb122a46fb5e7559c012fbb9d0d951533d13
caiosc1 <caio17.sc@gmail.com>
2024-10-01 17:03:26 -0300

    Update server.ts
    Update da rota '/signUp' e criação da rota '/login'

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -9,45 +9,85 @@ app.use(express.json());
 
 type Conta = {
     nome: string;
-    cpf: string;
+    data_nasc: string;
     email: string;
     senha: string;
 };
 
 let contas: Conta[] = [];
 
+function consultarEmail(email: string) : boolean{
+    for(var i = 0; i < contas.length; i++){
+        if (contas[i].email == email){
+            return false;
+        }
+    }
+    return true;
+}
+
 function cadastrarConta(conta: Conta) : number{
     contas.push(conta);
     return contas.length;
 }
 
+
+function loginConta(email: string, senha: string) : boolean{
+    for(var i = 0; i < contas.length; i++){
+        if (contas[i].email === email){
+            if (contas[i].senha === senha){
+                return true;
+            }
+            return false;
+        }
+    }
+    return false;
+} 
+
 route.put('/signUp', 
     (req: Request, res: Response)=>{
-        //quando usamos req.get queremos bter o parametro no HEADER da requisição
         const pnome = req.get('nome');
-        const pcpf = req.get('cpf');
+        const pdatanasc = req.get('data_nasc');
         const pemail = req.get('email');
         const psenha = req.get('senha');
 
-        if(pnome && pcpf && pemail && psenha){
+        if(pnome && pdatanasc && pemail && psenha){
             //proseguir com o cadastro
-            const novaConta: Conta = {
-                nome: pnome,
-                cpf: pcpf,
-                email: pemail,
-                senha: psenha
-            };
-            const codConta:number = cadastrarConta(novaConta);
-            res.send(`Nova conta adicionada ${codConta}`);
+            if (consultarEmail(pemail)){
+                const novaConta: Conta = {
+                    nome: pnome,
+                    data_nasc: pdatanasc,
+                    email: pemail,
+                    senha: psenha
+                };
+                const codConta:number = cadastrarConta(novaConta);
+                res.send(`Nova conta adicionada ${codConta}`);
+            }else{
+                res.send(`Email já cadastrado.`);
+            }
+          
         }else {
             res.send('Faltam parâmetros na requisição');
         }
     }
 );
 
-route.put('/login', 
+route.get('/login', 
     (req: Request, res: Response)=>{
-        
+        const pemail = req.get('email');
+        const psenha = req.get('senha');
+
+        if(pemail && psenha){
+            //proseguir com o login
+            if (loginConta(pemail, psenha)){
+                res.send(`Acesso Concedido!`);
+
+            }else{
+                res.send(`Credenciais Invalidas.`);
+            }
+          
+        }else {
+            res.send('Faltam parâmetros na requisição');
+        }
     }
 );
 
