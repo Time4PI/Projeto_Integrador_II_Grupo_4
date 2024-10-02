8bb82e2f21cb880f173e8c71e1de5a84d15f0dab
caiosc1 <caio17.sc@gmail.com>
2024-10-02 14:41:48 -0300

    Update accounts.ts

--- Backend/src/accounts/accounts.ts
+++ Backend/src/accounts/accounts.ts
@@ -1,13 +1,7 @@
 import {Request, RequestHandler, Response} from "express";
 
-/*
-    Nampespace que contém tudo sobre "contas de usuários"
-*/
 export namespace AccountsHandler {
     
-    /**
-     * Tipo UserAccount
-     */
     export type UserAccount = {
         name:string;
         email:string;
@@ -15,14 +9,8 @@ export namespace AccountsHandler {
         birthdate:string; 
     };
 
-    // Array que representa uma coleção de contas. 
     let accountsDatabase: UserAccount[] = [];
 
-    /**
-     * Salva uma conta no banco de dados. 
-     * @param ua conta de usuário do tipo @type {UserAccount}
-     * @returns @type { number } o código da conta cadastrada como posição no array.
-     */
     export function saveNewAccount(ua: UserAccount) : number{
         accountsDatabase.push(ua);
         return accountsDatabase.length;
@@ -35,20 +23,16 @@ export namespace AccountsHandler {
         }
         return false;
     }
-    /**
-     * Função para tratar a rota HTTP /signUp. 
-     * @param req Requisição http tratada pela classe @type { Request } do express
-     * @param res Resposta http a ser enviada para o cliente @type { Response }
-     */
+    
     export const signUpHandler: RequestHandler = (req: Request, res: Response) => {
-        // Passo 1 - Receber os parametros para criar a conta
+        
         const pName = req.get('name');
         const pEmail = req.get('email');
         const pPassword = req.get('password');
         const pBirthdate = req.get('birthdate');
         
         if(pName && pEmail && pPassword && pBirthdate){
-            // prosseguir com o cadastro... 
+            
             if (validateEmail(pEmail)){
                 const newAccount: UserAccount = {
                     name: pName,
